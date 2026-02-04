71e540a6ea00fbebd8aeffa0afd4f2e0dbeb975e
nathanael02vs <nathanael4@student.umn.ac.id>
2026-02-04 15:54:41 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000105359_NathanaelValentinoSutanto/week01/GameStore.kt
+++ src/oop_00000105359_NathanaelValentinoSutanto/week01/GameStore.kt
@@ -3,4 +3,13 @@ package oop_00000105359_NathanaelValentinoSutanto.week01
 fun main() {
     val gameTitle: String = "Zuma's Revenge"
     val price: Int = 750000
+
+    val finalPrice = calculateDiscount(price)
+
+}
+
+fun calculateDiscount(price: Int): Int = if (price > 500000) {
+    price - (price * 20 / 100)
+} else {
+    price - (price * 10 / 100)
 }
