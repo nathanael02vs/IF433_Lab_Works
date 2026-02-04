93cc6fb8223d348462bbec5dae82d96a94b19a44
nathanael02vs <nathanael4@student.umn.ac.id>
2026-02-04 16:05:39 +0700

    week01: (tugas 2) implement null safety for user note

--- src/oop_00000105359_NathanaelValentinoSutanto/week01/GameStore.kt
+++ src/oop_00000105359_NathanaelValentinoSutanto/week01/GameStore.kt
@@ -3,10 +3,13 @@ package oop_00000105359_NathanaelValentinoSutanto.week01
 fun main() {
     val gameTitle: String = "Zuma's Revenge"
     val price: Int = 750000
+    val userNote: String? = null
 
     val finalPrice = calculateDiscount(price)
 
-    printReceipt(title = gameTitle, originalPrice = price, finalPrice = finalPrice)
+    val noteText = userNote ?: "Tidak ada catatan"
+
+    printReceipt(title = gameTitle, originalPrice = price, finalPrice = finalPrice, note = noteText)
 }
 
 fun calculateDiscount(price: Int): Int = if (price > 500000) {
@@ -15,10 +18,11 @@ fun calculateDiscount(price: Int): Int = if (price > 500000) {
     price - (price * 10 / 100)
 }
 
-fun printReceipt(title: String, originalPrice: Int, finalPrice: Int){
+fun printReceipt(title: String, originalPrice: Int, finalPrice: Int, note: String){
     println("=== STEAMKW RECEIPT ===")
     println("Title: $title")
     println("Price: $originalPrice")
     println("Final Price: $finalPrice")
+    println("Note: $note")
     println("=======================")
 }
