2f338905d29aedf41b4540f7bb22da8226b7f37d
nathanael02vs <nathanael4@student.umn.ac.id>
2026-02-04 15:24:11 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000105359_NathanaelValentinoSutanto/week01/CircleCalculator.kt
+++ src/oop_00000105359_NathanaelValentinoSutanto/week01/CircleCalculator.kt
@@ -1,17 +1,11 @@
 package oop_00000105359_NathanaelValentinoSutanto.week01
 
-fun main(args: Array<String>){
-    //Variable definition
+fun main(){
     val radius = 7.0
     val pi = 3.14
-
-    //Calculation
     val area = pi * radius * radius
 
-    //Output concatenation
     println("Radius: $radius, Area: $area")
-
-    //Logic check
     println(checkSize(area))
 }
 
