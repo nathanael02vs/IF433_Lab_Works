9f8333cfd096e7f5807cf09bfa5e982083bd69a8
nathanael02vs <nathanael4@student.umn.ac.id>
2026-02-11 19:50:32 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_00000105359_NathanaelValentinoSutanto/week02/Student.kt
+++ src/oop_00000105359_NathanaelValentinoSutanto/week02/Student.kt
@@ -1,6 +1,11 @@
 package oop_00000105359_NathanaelValentinoSutanto.week02
 
-class Student(val name: String, val nim: String, var major: String) {
+class Student(
+    val name: String,
+    val nim: String,
+    var major: String,
+    var gpa: Double = 0.0
+) {
     init {
         // Validasi Sederhana: Cek panjang NIM
         if(nim.length != 5){
