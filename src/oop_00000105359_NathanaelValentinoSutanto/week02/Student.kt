44023680f57d3097606ba7040f16dc1d81747605
nathanael02vs <nathanael4@student.umn.ac.id>
2026-02-11 19:21:14 +0700

    week02: add warning validation in init block

--- src/oop_00000105359_NathanaelValentinoSutanto/week02/Student.kt
+++ src/oop_00000105359_NathanaelValentinoSutanto/week02/Student.kt
@@ -1,9 +1,13 @@
 package oop_00000105359_NathanaelValentinoSutanto.week02
 
-class Student(
-    val name: String,
-    val nim: String,
-    var major: String
-) {
-    //Body class kosong dulu
+class Student(val name: String, val nim: String, var major: String) {
+    init {
+        // Validasi Sederhana: Cek panjang NIM
+        if(nim.length != 5){
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name barhasil dialokasikan di Memory.")
+        }
+    }
 }
