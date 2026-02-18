4ef22ac442c095cdb38455ec1f76668b92d805da
nathanael02vs <nathanael4@student.umn.ac.id>
2026-02-18 10:41:47 +0700

    week03: cleanup code

--- src/oop_00000105359_NathanaelValentinoSutanto/week03/Employee.kt
+++ src/oop_00000105359_NathanaelValentinoSutanto/week03/Employee.kt
@@ -5,13 +5,12 @@ class Employee(val name: String){
         set(value){
             if (value < 0){
                 println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
-                field = 0 // Gunakan field, bukan this.salary
+                field = 0
             } else {
-                field = value // Gunakan field untuk assign nilai asli
+                field = value
             }
         }
 
-    // Hanya bisa diakses di file/class ini saja
     private var performanceRating: Int = 3
 
     fun increasePerformance(){
@@ -19,13 +18,10 @@ class Employee(val name: String){
         println("Kinerja $name meningkat! Rating: $performanceRating")
     }
 
-    //Kita tidak buat getter untuk performanceRating, jadi data ini benar-benar rahasia
-    //Kecuali kita buat function khusus untuk print
     fun printStatus(){
         println("Karyawan: $name, Rating: $performanceRating")
     }
 
-    //Tax (pajak) dihitung 10% dari gaji saat ini
     val tax: Double
         get() = salary * 0.1
 }
--- src/oop_00000105359_NathanaelValentinoSutanto/week03/Main.kt
+++ src/oop_00000105359_NathanaelValentinoSutanto/week03/Main.kt
@@ -3,16 +3,10 @@ package oop_00000105359_NathanaelValentinoSutanto.week03
 fun main(){
     val e = Employee("Budi")
 
-    //1. Test Validasi Salary
-    e.salary = -1000 //Harusnya print Error
+    e.salary = -1000
     e.salary = 5000000
     println("Gaji: $${e.salary}")
 
-    //2. Test Encapsulation
     e.increasePerformance()
-    //e.performanceRating = 5 //Di uncomment jadi merah (error)
-
-    //3. Test Computed Property
     println("Pajak yang harus dibayar: ${e.tax}")
-
 }
