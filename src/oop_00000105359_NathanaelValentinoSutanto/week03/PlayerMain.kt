c9baa97d8240200354c04d52ea40373133c018de
nathanael02vs <nathanael4@student.umn.ac.id>
2026-02-18 11:16:55 +0700

    week03: (task 2) implement Player testing in main

---
+++ src/oop_00000105359_NathanaelValentinoSutanto/week03/PlayerMain.kt
@@ -0,0 +1,9 @@
+package oop_00000105359_NathanaelValentinoSutanto.week03
+
+fun main() {
+    println("=== Test Player ===")
+    val player = Player("Nathanael")
+
+    player.addXp(50)
+    player.addXp(60)
+}
