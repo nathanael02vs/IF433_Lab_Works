cc5c659c1f56e1e16b12b8cc4b65a90749410ef8
nathanael02vs <nathanael4@student.umn.ac.id>
2026-02-25 10:50:43 +0700

    week04: (task 2) create Manager class overriding Employee

---
+++ src/oop_00000105359_NathanaelValentinoSutanto/week04/Manager.kt
@@ -0,0 +1,12 @@
+package oop_00000105359_NathanaelValentinoSutanto.week04
+
+class Manager(name: String, baseSalary: Int) : Employee(name, baseSalary) {
+
+    override fun work() {
+        println("$name sedang memimpin rapat divisi.")
+    }
+
+    override fun calculateBonus(): Int {
+        return super.calculateBonus() + 500000
+    }
+}
