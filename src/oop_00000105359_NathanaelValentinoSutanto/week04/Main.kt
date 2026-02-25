7ce715e4c0d801c3063a66579286ede62084b3b4
nathanael02vs <nathanael4@student.umn.ac.id>
2026-02-25 10:46:52 +0700

    week04: (task 1) test ElectricCar in main

--- src/oop_00000105359_NathanaelValentinoSutanto/week04/Main.kt
+++ src/oop_00000105359_NathanaelValentinoSutanto/week04/Main.kt
@@ -11,4 +11,10 @@ fun main(){
     myCar.openTrunk() //Memanggil method milik sendiri
     myCar.honk()      //Memanggil method yang sudah di-override
     myCar.accelerate()//Memanggil gabungan method Parent dan Child
+
+    println("\n--- Testing ElectricCar ---")
+    val myElectricCar = ElectricCar(brand = "Tesla", numberOfDoors = 4, batteryCapacity = 80)
+    myElectricCar.accelerate()
+    myElectricCar.honk()
+    myElectricCar.openTrunk()
 }
