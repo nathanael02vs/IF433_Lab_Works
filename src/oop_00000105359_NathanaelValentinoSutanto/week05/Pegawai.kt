042e5f7ed261d106aad1b8e57e078fd6b7a449b9
nathanael02vs <nathanael4@student.umn.ac.id>
2026-03-04 10:17:39 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_00000105359_NathanaelValentinoSutanto/week05/Pegawai.kt
@@ -0,0 +1,6 @@
+package oop_00000105359_NathanaelValentinoSutanto.week05
+
+abstract class Pegawai(val nama: String){
+    //Abstract function tidak memiliki body (tanpa kurung kurawal)
+    abstract fun bekerja()
+}
