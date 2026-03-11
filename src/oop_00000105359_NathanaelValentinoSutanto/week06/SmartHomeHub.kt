4f95b1c542d54c9c59ebc10dd39c6cf271464179
nathanael02vs <nathanael4@student.umn.ac.id>
2026-03-11 10:27:20 +0700

    week06: (task) implement advanced smart casting in Hub

--- src/oop_00000105359_NathanaelValentinoSutanto/week06/SmartHomeHub.kt
+++ src/oop_00000105359_NathanaelValentinoSutanto/week06/SmartHomeHub.kt
@@ -16,4 +16,16 @@ class SmartHomeHub {
             }
         }
     }
+
+    fun activateSecurityMode() {
+        println("\n[Hub] MODE KEAMANAN DIAKTIFKAN!")
+        for (device in devices) {
+            if (device is Recordable) {
+                device.startRecord()
+            }
+            if (device is SmartSpeaker) {
+                device.playMusic("Sirine Peringatan")
+            }
+        }
+    }
 }
