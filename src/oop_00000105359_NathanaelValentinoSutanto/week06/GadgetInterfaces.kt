006a485c25b5507b078a79d6b44e3efbee0bdc5d
nathanael02vs <nathanael4@student.umn.ac.id>
2026-03-11 09:55:16 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000105359_NathanaelValentinoSutanto/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000105359_NathanaelValentinoSutanto.week06
+
+interface Camera {
+    fun turnOn(){
+        println("Lensa kamera terbuka dan sensor aktif")
+    }
+}
+
+interface Phone {
+    fun turnOn(){
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
