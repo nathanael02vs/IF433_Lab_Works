26f51fa320503f7bd645169dded583cdeeb54913
nathanael02vs <nathanael4@student.umn.ac.id>
2026-03-11 09:41:12 +0700

    week06: create compound interfaces

---
+++ src/oop_00000105359_NathanaelValentinoSutanto/week06/Capabilities.kt
@@ -0,0 +1,10 @@
+package oop_00000105359_NathanaelValentinoSutanto.week06
+
+// Interface = Can-Do (Behavior)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+}
