e967151a5e9d0f8304e2be439fba531be838464c
nathanael02vs <nathanael4@student.umn.ac.id>
2026-03-11 09:44:07 +0700

    week06: create Smartwatch with multiple implementations

---
+++ src/oop_00000105359_NathanaelValentinoSutanto/week06/Smartwatch.kt
@@ -0,0 +1,16 @@
+package oop_00000105359_NathanaelValentinoSutanto.week06
+
+// Menggabungkan 1 Parent Class dan 2 Interfaces
+class Smartwatch: Watch(), BluetoothConnectable, Rechargeable {
+    override fun showTime(){
+        println("Layar OLED menyala: 14:00 WIB")
+    }
+
+    override fun connectToBluetooth(){
+        println("Mencari perangkat HP di sekitar untuk pairing...")
+    }
+
+    override fun chargeBattery(){
+        println("Mengisi daya menggunakan charger magnetik 15W")
+    }
+}
