da7e4f78e6eab3e563679ae91e7231cfc83d5bda
nathanael02vs <nathanael4@student.umn.ac.id>
2026-03-25 10:43:28 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000105359_NathanaelValentinoSutanto/week07/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000105359_NathanaelValentinoSutanto.week07
+
+enum class ItemRarity(val dropChance: Int){
+    COMMON(70),
+    UNCOMMON(40),
+    RARE(15),
+    EPIC(5),
+    LEGENDARY(1)
+}
