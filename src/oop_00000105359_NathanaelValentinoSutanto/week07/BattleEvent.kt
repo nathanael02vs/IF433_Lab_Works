9f123cf8ccb2f54d676eaabd468c9e3f16f8ceea
nathanael02vs <nathanael4@student.umn.ac.id>
2026-03-25 10:53:07 +0700

    week07: (task) create BattleState sealed class hierarchy

---
+++ src/oop_00000105359_NathanaelValentinoSutanto/week07/BattleEvent.kt
@@ -0,0 +1,10 @@
+package oop_00000105359_NathanaelValentinoSutanto.week07
+
+import javax.management.BadAttributeValueExpException
+
+sealed class BattleState{
+    data class MonsterEncounter(val monsterName: String): BattleState()
+    data class LootDropped(val item: GameItem): BattleState()
+    data class GameOver(val reason: String): BattleState()
+    object SafeZone: BattleState()
+}
