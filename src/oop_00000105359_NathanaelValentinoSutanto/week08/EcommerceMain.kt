768367539d1bf3969999112f4933cc13905b0ec3
nathanael02vs <nathanael4@student.umn.ac.id>
2026-04-15 10:12:43 +0700

    week08: (task) process mixed raw JSON list using as?

---
+++ src/oop_00000105359_NathanaelValentinoSutanto/week08/EcommerceMain.kt
@@ -0,0 +1,27 @@
+package oop_00000105359_NathanaelValentinoSutanto.week08
+
+fun main() {
+    val rawApiData: List<Map<String, Any?>> = listOf(
+        mapOf("id" to "E01", "name" to "Laptop", "type" to "ELECTRONIC", "warranty" to 24),
+        mapOf("id" to "C01", "name" to "T-Shirt", "type" to "CLOTHING", "size" to "XL"),
+        mapOf("id" to "E02", "name" to "Mouse", "type" to "ELECTRONIC", "warranty" to "Not An Integer"),
+        mapOf("name" to "Ghost Item", "type" to "CLOTHING"), // Missing ID!
+        mapOf("id" to "X01", "name" to "Unknown", "type" to "FOOD") // Unknown type
+    )
+
+    val parser = ApiParser()
+
+    for (raw in rawApiData) {
+        try {
+            val product = parser.parseProduct(raw)
+
+            product?.let {
+                println("Produk ditemukan: $it")
+                parser.checkout(it)
+            } ?: println("Tipe produk tidak dikenal, di-skip.")
+
+        } catch (e: IllegalArgumentException) {
+            println("PERINGATAN: Data korup! ${e.message}")
+        }
+    }
+}
