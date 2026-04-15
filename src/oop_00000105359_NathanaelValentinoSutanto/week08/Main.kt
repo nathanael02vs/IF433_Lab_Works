cf2fe555e6bfac9d80edc1e6d47b064efa311bda
nathanael02vs <nathanael4@student.umn.ac.id>
2026-04-15 09:56:44 +0700

    week08: implement safe casting as? to filter collection

--- src/oop_00000105359_NathanaelValentinoSutanto/week08/Main.kt
+++ src/oop_00000105359_NathanaelValentinoSutanto/week08/Main.kt
@@ -27,4 +27,12 @@ fun main() {
         "Laptop",
         4500000.0
     )
+    for (item in mixedData) {
+        val text = item as? String
+
+        // Hanya cetak jika cast sukses (text tidak null)
+        text?.let {
+            println("Ditemukan teks: ${it.uppercase()}")
+        }
+    }
 }
