68cea08b92394555c61cf2dbb8af5b1bdc514d7f
nathanael02vs <nathanael4@student.umn.ac.id>
2026-04-15 10:11:25 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000105359_NathanaelValentinoSutanto/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000105359_NathanaelValentinoSutanto.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
