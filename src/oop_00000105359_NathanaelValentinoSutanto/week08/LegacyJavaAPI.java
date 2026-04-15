2323b78e0ad9beecd27eaf3c50e53475cbc010f1
nathanael02vs <nathanael4@student.umn.ac.id>
2026-04-15 10:04:20 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_00000105359_NathanaelValentinoSutanto/week08/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package oop_00000105359_NathanaelValentinoSutanto.week08;
+
+public class LegacyJavaAPI {
+    // Fungsi ini ditulis dalam Java. Kotlin tidak tahu apakah kembaliannya Nullable.
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
