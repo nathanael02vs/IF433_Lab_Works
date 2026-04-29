40d49af2232dedc4b9140de5e306ec7efbb073e6
nathanael02vs <nathanael4@student.umn.ac.id>
2026-04-29 09:53:56 +0700

    week10: (task) populate coin repository

--- src/oop_00000105359_NathanaelValentinoSutanto/week10/CryptoDashboard.kt
+++ src/oop_00000105359_NathanaelValentinoSutanto/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_00000105359_NathanaelValentinoSutanto.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 2.0))
+    coinRepo.add(Coin("USDT", 1000.0))
 }
