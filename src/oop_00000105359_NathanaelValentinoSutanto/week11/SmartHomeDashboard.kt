415dfd199fbed07f9604d72f82178bd6bf4a8263
nathanael02vs <nathanael4@student.umn.ac.id>
2026-05-06 10:03:20 +0700

    week11: (task) test full smart home configuration pipeline

--- src/oop_00000105359_NathanaelValentinoSutanto/week11/SmartHomeDashboard.kt
+++ src/oop_00000105359_NathanaelValentinoSutanto/week11/SmartHomeDashboard.kt
@@ -36,4 +36,6 @@ fun main() {
 
     val totalPower = homeDevices.run { sumOf { it.powerLoad } }
     println("Total daya: $totalPower Watt")
+
+    homeDevices.forEach { println(it.diagnose()) }
 }
