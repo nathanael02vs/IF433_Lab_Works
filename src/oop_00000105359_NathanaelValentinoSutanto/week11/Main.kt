6b32dfdd88c3dc39904e7454db678f9a5593a1fd
nathanael02vs <nathanael4@student.umn.ac.id>
2026-05-06 09:48:17 +0700

    week11: test extension functions in main

---
+++ src/oop_00000105359_NathanaelValentinoSutanto/week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_00000105359_NathanaelValentinoSutanto.week11
+
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
