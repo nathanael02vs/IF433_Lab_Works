3861016526c6b345d50aa63b817f534ee49fe968
nathanael02vs <nathanael4@student.umn.ac.id>
2026-05-06 10:02:06 +0700

    week11: (task) calculate total power usage

--- src/oop_00000105359_NathanaelValentinoSutanto/week11/SmartHomeDashboard.kt
+++ src/oop_00000105359_NathanaelValentinoSutanto/week11/SmartHomeDashboard.kt
@@ -33,4 +33,7 @@ fun main() {
     with(homeDevices) {
         println("Total perangkat terdaftar: ${this.size}")
     }
+
+    val totalPower = homeDevices.run { sumOf { it.powerLoad } }
+    println("Total daya: $totalPower Watt")
 }
