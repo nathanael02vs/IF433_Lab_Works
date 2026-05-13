4bf38af748ecf0905f20cff530aa8f1a21ecc878
nathanael02vs <nathanael4@student.umn.ac.id>
2026-05-13 10:45:12 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_00000105359_NathanaelValentinoSutanto/week12/SmartFeeder.kt
+++ src/oop_00000105359_NathanaelValentinoSutanto/week12/SmartFeeder.kt
@@ -2,4 +2,7 @@ package oop_00000105359_NathanaelValentinoSutanto.week12
 
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+    if (isJammed) {
+        throw DispenserJamException()
+    }
 }
