87aca025e16246279726540ae6a1101881619a9e
nathanael02vs <nathanael4@student.umn.ac.id>
2026-05-13 10:41:56 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_00000105359_NathanaelValentinoSutanto/week12/SmartFeeder.kt
@@ -0,0 +1,5 @@
+package oop_00000105359_NathanaelValentinoSutanto.week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+}
