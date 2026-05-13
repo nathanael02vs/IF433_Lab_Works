2bf8e691306775c4874d38b2b7eaf453040ee20f
nathanael02vs <nathanael4@student.umn.ac.id>
2026-05-13 10:47:12 +0700

    week12: (task) test full robust smart feeder pipeline

--- src/oop_00000105359_NathanaelValentinoSutanto/week12/SmartFeeder.kt
+++ src/oop_00000105359_NathanaelValentinoSutanto/week12/SmartFeeder.kt
@@ -1,5 +1,5 @@
 package oop_00000105359_NathanaelValentinoSutanto.week12
-
+// Checkpoint 20
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
     if (isJammed) {
