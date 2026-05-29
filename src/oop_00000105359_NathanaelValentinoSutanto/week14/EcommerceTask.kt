11780761873942ed9a1992d11da2f0f0f07f5813
nathanael02vs <nathanael4@student.umn.ac.id>
2026-05-29 12:26:22 +0700

    week14: (task) implement PricingStrategy interface for OCP

--- src/oop_00000105359_NathanaelValentinoSutanto/week14/EcommerceTask.kt
+++ src/oop_00000105359_NathanaelValentinoSutanto/week14/EcommerceTask.kt
@@ -46,16 +46,24 @@ class EmailNotifier : NotificationService {
     }
 }
 
+interface PricingStrategy {
+    fun calculate(price: Double): Double
+}
+
+class RegularPricing : PricingStrategy {
+    override fun calculate(price: Double) = price
+}
+
+class VipPricing : PricingStrategy {
+    override fun calculate(price: Double) = price * 0.90
+}
+
 class SafeOrderProcessor(
     private val repo: OrderRepository,
     private val notifier: NotificationService
 ) {
-    fun processOrder(itemName: String, basePrice: Double, customerType: String) {
-        val finalPrice = when (customerType) {
-            "REGULAR" -> basePrice
-            "VIP" -> basePrice * 0.90
-            else -> basePrice
-        }
+    fun processOrder(itemName: String, basePrice: Double, pricing: PricingStrategy, customerType: String) {
+        val finalPrice = pricing.calculate(basePrice)
 
         println("Memproses pesanan $itemName seharga $finalPrice")
         repo.saveOrder(itemName, finalPrice, customerType)
